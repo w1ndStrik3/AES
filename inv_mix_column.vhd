7c68abd5eb6e981389dc549c2254aa89b324a729
w1ndStrik3 <s214683@student.dtu.dk>
2024-01-18 10:23:56 +0100

    file renaming

--- inv_mix_column.vhd
+++ inv_mix_column.vhd
@@ -1,97 +1,100 @@
--- This program adds each byte of state is with a byte of round key.
+--this module recives 4 bytes (a column), and sends it to in_mult one byte at a time, where the byte gets multiplied with 9, 11, 13 and 14. afterwards 
+-- the output bytes is found by using XOR gates with the multiplied bytes
+ 
 
--- Completion time: 1 cycle.
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
-
-entity inv_mix_column is
-    port ( 
-		input_imc : in std_logic_vector(31 downto 0);
-        output_imc : out std_logic_vector(31 downto 0);
-		clk : in std_logic
-	);
+entity in_Mix_column is
+     Port(input_byte: in STD_LOGIC_Vector(31 downto 0);
+        output_byte : out STD_LOGIC_Vector(31 downto 0);
+		clk : in std_logic);
 		
-end entity inv_mix_column;
+end entity in_Mix_column;
 
-architecture behavioral of inv_mix_column is
+architecture Behavioral of in_Mix_column is
 
-	component inverse_mult is
-		port (
-			clk : in std_logic;
-			input : in std_logic_vector(7 downto 0);
-			output_9 : out std_logic_vector(7 downto 0);
-			output_11 : out std_logic_vector(7 downto 0);
-			output_13 : out std_logic_vector(7 downto 0);
-			output_14 : out std_logic_vector(7 downto 0)
-			);
-			
-	end component;
 
-	signal byte0x9 : std_logic_vector(7 downto 0);
-	signal byte0x11 : std_logic_vector(7 downto 0);
-	signal byte0x13 : std_logic_vector(7 downto 0);
-	signal byte0x14 : std_logic_vector(7 downto 0);
+component inverse_mult is -- Declare the componet that is used by this module
+	Port(
+	     clk : in std_logic;
+	     input: in STD_LOGIC_Vector(7 downto 0);
+	     output_9 : out STD_LOGIC_Vector(7 downto 0);
+	     output_11 : out STD_LOGIC_Vector(7 downto 0);
+	     output_13 : out STD_LOGIC_Vector(7 downto 0);
+	     output_14 : out STD_LOGIC_Vector(7 downto 0)
+          );
+		
+end component;
 
-	signal byte1x9 : std_logic_vector(7 downto 0);
-	signal byte1x11 : std_logic_vector(7 downto 0);
-	signal byte1x13 : std_logic_vector(7 downto 0);
-	signal byte1x14 : std_logic_vector(7 downto 0);
+signal byte0x9 : STD_LOGIC_Vector(7 downto 0); -- input(7 downto 0) multiplied with 9
+signal byte0x11 : STD_LOGIC_Vector(7 downto 0);
+signal byte0x13 : STD_LOGIC_Vector(7 downto 0);
+signal byte0x14 : STD_LOGIC_Vector(7 downto 0);
 
-	signal byte2x9 : std_logic_vector(7 downto 0);
-	signal byte2x11 : std_logic_vector(7 downto 0);
-	signal byte2x13 : std_logic_vector(7 downto 0);
-	signal byte2x14 : std_logic_vector(7 downto 0);
+signal byte1x9 : STD_LOGIC_Vector(7 downto 0);
+signal byte1x11 : STD_LOGIC_Vector(7 downto 0);
+signal byte1x13 : STD_LOGIC_Vector(7 downto 0);
+signal byte1x14 : STD_LOGIC_Vector(7 downto 0);
 
-	signal byte3x9 : std_logic_vector(7 downto 0);
-	signal byte3x11 : std_logic_vector(7 downto 0);
-	signal byte3x13 : std_logic_vector(7 downto 0);
-	signal byte3x14 : std_logic_vector(7 downto 0);
+signal byte2x9 : STD_LOGIC_Vector(7 downto 0);
+signal byte2x11 : STD_LOGIC_Vector(7 downto 0);
+signal byte2x13 : STD_LOGIC_Vector(7 downto 0);
+signal byte2x14 : STD_LOGIC_Vector(7 downto 0);
+
+signal byte3x9 : STD_LOGIC_Vector(7 downto 0);
+signal byte3x11 : STD_LOGIC_Vector(7 downto 0);
+signal byte3x13 : STD_LOGIC_Vector(7 downto 0);
+signal byte3x14 : STD_LOGIC_Vector(7 downto 0);
+
+
 
 begin
 
-byte0_mult : inverse_mult
+byte0_mult : inverse_mult --first byte gets multiplied
 		port map(
-			 input => input_imc(7 downto 0),
+			 input => input_byte(31 downto 24),
 			 output_9 => byte0x9(7 downto 0),
                          output_11 => byte0x11(7 downto 0),
                          output_13 => byte0x13(7 downto 0),
                          output_14 => byte0x14(7 downto 0),
 			 clk => clk);	
 
-byte1_mult : inverse_mult
+byte1_mult : inverse_mult --second byte gets multiplied
 		port map(
-			 input => input_imc(15 downto 8),
+			 input => input_byte(23 downto 16),
 			 output_9 => byte1x9(7 downto 0),
                          output_11 => byte1x11(7 downto 0),
                          output_13 => byte1x13(7 downto 0),
                          output_14 => byte1x14(7 downto 0),
 			 clk => clk);
 
-byte2_mult : inverse_mult
+byte2_mult : inverse_mult --third byte gets multiplied
 		port map(
-			 input => input_imc(23 downto 16),
+			 input => input_byte(15 downto 8),
 			 output_9 => byte2x9(7 downto 0),
                          output_11 => byte2x11(7 downto 0),
                          output_13 => byte2x13(7 downto 0),
                          output_14 => byte2x14(7 downto 0),
 			 clk => clk);
 
-byte3_mult : inverse_mult
+byte3_mult : inverse_mult --fourth byte gets multiplied
 		port map(
-			 input => input_imc(31 downto 24),
+			 input => input_byte(7 downto 0),
 			 output_9 => byte3x9(7 downto 0),
                          output_11 => byte3x11(7 downto 0),
                          output_13 => byte3x13(7 downto 0),
                          output_14 => byte3x14(7 downto 0),
 			 clk => clk);
 
-output_imc(7 downto 0) <= byte0X14 XOR byte1x11 xor byte2x13 xor byte3x9;
-output_imc(15 downto 8) <= byte0X9 XOR byte1x14 xor byte2x11 xor byte3x13;
-output_imc(23 downto 16) <= byte0X13 XOR byte1x9 xor byte2x14 xor byte3x11;
-output_imc(31 downto 24) <= byte0X11 XOR byte1x13 xor byte2x9 xor byte3x14;
+output_byte(31 downto 24) <= byte0X14 XOR byte1x11 xor byte2x13 xor byte3x9; -- first output byte is found using XOR gates
+output_byte(23 downto 16) <= byte0X9 XOR byte1x14 xor byte2x11 xor byte3x13;
+output_byte(15 downto 8) <= byte0X13 XOR byte1x9 xor byte2x14 xor byte3x11;
+output_byte(7 downto 0) <= byte0X11 XOR byte1x13 xor byte2x9 xor byte3x14;
 
-end architecture;
+
+
+end Behavioral;
--- inv_shift_rows.vhd
+++ inv_shift_rows.vhd
@@ -1,42 +1,48 @@
--- This program shifts bytes in each row of state to the right. 
+-- Shifts bytes in each row of state to the left. 
 -- Row 0 unchanged, row 1 shifts one, row 2 shifts two, row 3 shifts three.
 
--- Completion time: 1 cycle.
+-- Completion time: 
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity inv_shift_rows is
+entity in_shift_rows is
     port (
-        input_isr : in std_logic_vector(127 downto 0);
-        output_isr : out std_logic_vector(127 downto 0);
+        input_byte : in std_logic_vector(127 downto 0);
+        output_byte : out std_logic_vector(127 downto 0);
         clk : in std_logic
     );
-end inv_shift_rows;
+end in_shift_rows;
 
-architecture behavioral of inv_shift_rows is
+architecture behavioral of in_shift_rows is
+
+
+
 begin
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            output_isr(0*8+7 downto 0*8) <= input_isr(0*8+7 downto 0*8);
-            output_isr(1*8+7 downto 1*8) <= input_isr(13*8+7 downto 13*8);
-            output_isr(2*8+7 downto 2*8) <= input_isr(10*8+7 downto 10*8);
-            output_isr(3*8+7 downto 3*8) <= input_isr(7*8+7 downto 7*8);
-            output_isr(4*8+7 downto 4*8) <= input_isr(4*8+7 downto 4*8);
-            output_isr(5*8+7 downto 5*8) <= input_isr(1*8+7 downto 1*8);
-            output_isr(6*8+7 downto 6*8) <= input_isr(14*8+7 downto 14*8);
-            output_isr(7*8+7 downto 7*8) <= input_isr(11*8+7 downto 11*8);
-            output_isr(8*8+7 downto 8*8) <= input_isr(8*8+7 downto 8*8);
-            output_isr(9*8+7 downto 9*8) <= input_isr(5*8+7 downto 5*8);
-            output_isr(10*8+7 downto 10*8) <= input_isr(2*8+7 downto 2*8);
-            output_isr(11*8+7 downto 11*8) <= input_isr(15*8+7 downto 15*8);
-            output_isr(12*8+7 downto 12*8) <= input_isr(12*8+7 downto 12*8);
-            output_isr(13*8+7 downto 13*8) <= input_isr(9*8+7 downto 9*8);
-            output_isr(14*8+7 downto 14*8) <= input_isr(6*8+7 downto 6*8);
-            output_isr(15*8+7 downto 15*8) <= input_isr(3*8+7 downto 3*8);  
+process(clk)
+begin
+	if rising_edge(clk) then
+			output_byte(15*8+7 downto 15*8) <= input_byte(15*8+7 downto 15*8);
+			output_byte(14*8+7 downto 14*8) <= input_byte(2*8+7 downto 2*8);
+			output_byte(13*8+7 downto 13*8) <= input_byte(5*8+7 downto 5*8);
+			output_byte(12*8+7 downto 12*8) <= input_byte(8*8+7 downto 8*8);
+			output_byte(11*8+7 downto 11*8) <= input_byte(11*8+7 downto 11*8);
+			output_byte(10*8+7 downto 10*8) <= input_byte(14*8+7 downto 14*8);
+			output_byte(9*8+7 downto 9*8) <= input_byte(1*8+7 downto 1*8);
+			output_byte(8*8+7 downto 8*8) <= input_byte(4*8+7 downto 4*8);
+                        output_byte(7*8+7 downto 7*8) <= input_byte(7*8+7 downto 7*8);
+                        output_byte(6*8+7 downto 6*8) <= input_byte(10*8+7 downto 10*8);
+                        output_byte(5*8+7 downto 5*8) <= input_byte(13*8+7 downto 13*8);
+                        output_byte(4*8+7 downto 4*8) <= input_byte(0*8+7 downto 0*8);
+                        output_byte(3*8+7 downto 3*8) <= input_byte(3*8+7 downto 3*8);
+                        output_byte(2*8+7 downto 2*8) <= input_byte(6*8+7 downto 6*8);
+                        output_byte(1*8+7 downto 1*8) <= input_byte(9*8+7 downto 9*8);
+                        output_byte(0*8+7 downto 0*8) <= input_byte(12*8+7 downto 12*8);  
+		
+		
+
 	end if;
 end process;
 
-end architecture;
+end behavioral;
